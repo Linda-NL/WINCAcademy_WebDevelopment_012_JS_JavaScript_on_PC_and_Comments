d472d6bf096db82675c30a52b268da67d29ece43
Linda <86129384+Linda-NL@users.noreply.github.com>
2021-07-20 22:50:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+console.log('Hallo Winc Academy studenten')
+
+/*
+//deel 2 opdracht 1: dit is een grote som
+let multiplication = 1230941 * 1823794;
+console.log('anser multiplication:', multiplication)
+
+//deel 2 opdracht 2
+let division = 637263 / 54;
+console.log('answer division', division)
+*/
+
+//deel 2 opdracht 3
+//gedaan
+
+//deel 2 opdracht 4
+//nee natuurlijk niet, het is een comment
+
+//deel 2 opdracht 5
+/* gedaan, en dit staat in een block quote */
+
+//deel 2 opdracht 6
+/* nee, natuurlijk zie je de uitkomst niet meer, 
+het staat in een block quote en node leest het niet meer als code */
